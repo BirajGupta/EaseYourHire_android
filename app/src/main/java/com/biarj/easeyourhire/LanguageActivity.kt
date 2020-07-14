6ca9f513cb2f5f60da9bc2ef173af4253e93d6e7
Rakshit <guptarakshit2000@gmail.com>
2020-07-15 00:36:58 +0530

    first commit

--- app/src/main/java/com/biarj/easeyourhire/LanguageActivity.kt
+++ app/src/main/java/com/biarj/easeyourhire/LanguageActivity.kt
@@ -1,12 +1,23 @@
 package com.biarj.easeyourhire
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class LanguageActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_language)
+
+        Handler().postDelayed({
+            val startAct = Intent(this@LanguageActivity, MainActivity::class.java)
+            startActivity(startAct)
+            finish()
+        },2000)
+        }
     }
-}
+
+
