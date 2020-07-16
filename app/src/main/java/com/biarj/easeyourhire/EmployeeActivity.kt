8e904c9ca119734cad94eab37dbb00684c711964
Rakshit <guptarakshit2000@gmail.com>
2020-07-16 14:10:01 +0530

    option created

--- app/src/main/java/com/biarj/easeyourhire/EmployeeActivity.kt
+++ app/src/main/java/com/biarj/easeyourhire/EmployeeActivity.kt
@@ -3,7 +3,7 @@ package com.biarj.easeyourhire
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class employee : AppCompatActivity() {
+class EmployeeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/biarj/easeyourhire/OptionActivity.kt
+++ app/src/main/java/com/biarj/easeyourhire/OptionActivity.kt
@@ -1,12 +1,30 @@
 package com.biarj.easeyourhire
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_option.*
 
 class OptionActivity : AppCompatActivity() {
-
+    lateinit var employer: Button
+    lateinit var employee: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_option)
+        employer=findViewById(R.id.employer)
+        employee=findViewById(R.id.employee)
+        employer.setOnClickListener{
+            val i = Intent(this, EmployerActivity::class.java)
+            startActivity(i)
+
+
+        }
+        employee.setOnClickListener{
+            val i = Intent(this, EmployeeActivity::class.java)
+            startActivity(i)
+
+
+        }
     }
 }
