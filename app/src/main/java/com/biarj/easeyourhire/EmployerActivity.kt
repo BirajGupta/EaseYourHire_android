d225c2f565d9f255bd8ed4afa3b0bbbbe6af9f87
BIraj <birag.gpta@gmail.com>
2020-07-18 00:11:49 +1200

    added recycler adapter in employer activity

--- app/src/main/java/com/biarj/easeyourhire/EmployerActivity.kt
+++ app/src/main/java/com/biarj/easeyourhire/EmployerActivity.kt
@@ -2,14 +2,37 @@ package com.biarj.easeyourhire
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Adapter
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class EmployerActivity : AppCompatActivity() {
+
+    lateinit var recyclerCatDetails: RecyclerView
+    lateinit var layoutManager: RecyclerView.LayoutManager
+    lateinit var catRecyclerAdapter : CatDetailsRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_employer)
 
-        
+        val categories : ArrayList<String> = arrayListOf("Rakshit", "ravish", "arteev", "Biraj")
+
+        recyclerCatDetails = findViewById(R.id.recyclerEmployer)
+
+        layoutManager = LinearLayoutManager(this@EmployerActivity)
+
+        catRecyclerAdapter = CatDetailsRecyclerAdapter(
+            this@EmployerActivity,
+            categories
+        )
+
+        recyclerCatDetails.adapter = catRecyclerAdapter
+
+        recyclerCatDetails.layoutManager = layoutManager
+
+
 
     }
 }
---
+++ app/src/main/java/com/biarj/easeyourhire/catDetailsRecyclerAdapter.kt
@@ -0,0 +1,42 @@
+package com.biarj.easeyourhire
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.TextureView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class CatDetailsRecyclerAdapter(context: Context, var categories:ArrayList<String>): RecyclerView.Adapter<CatDetailsRecyclerAdapter.ResDetailsViewHolder>(){
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CatDetailsRecyclerAdapter.ResDetailsViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.employer_single_recycler,parent, false)
+
+
+        return ResDetailsViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return categories.size
+    }
+
+    override fun onBindViewHolder(
+        holder: CatDetailsRecyclerAdapter.ResDetailsViewHolder,
+        position: Int
+    ) {
+        val category = categories[position]
+
+        holder.workerName.text = category
+    }
+
+
+    class ResDetailsViewHolder(view: View) : RecyclerView.ViewHolder(view){
+
+            var workerName: TextView = view.findViewById(R.id.txtPersonName)
+
+    }
+}
