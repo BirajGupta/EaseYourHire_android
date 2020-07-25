7790a4a98f20ae19b87a201e2d4c3b9d62414df6
Rakshit <guptarakshit2000@gmail.com>
2020-07-25 23:55:18 +0530

    registration

---
+++ app/src/main/java/com/biarj/easeyourhire/RequestActivity.kt
@@ -0,0 +1,12 @@
+package com.biarj.easeyourhire
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RequestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_request)
+        title="REQUEST GENERATION"
+    }
+}
